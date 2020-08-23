612d80772e713f4be4173a8f80f8aa3d8cbcbd64
Vlad Mocanu <moccacino@gmail.com>
2020-08-23 19:15:42 +0300

    -added configuration for filtering anomaly from sensors -added configuration for start and end year to generate reports from -added more comments -uniform double quotes everywhere -added function to encapsulate title page, sample statistics table and temepereture histograms

--- chart_functions.py
+++ chart_functions.py
@@ -1,61 +1,107 @@
 import matplotlib.pyplot as plt
 
+# initialize chart with sizes, title and labels
 def init_chart(title, xlabel, ylabel, xsize, ysize):
     fig, ax = plt.subplots(figsize=(xsize, ysize))
     ax.set(title = title, ylabel = ylabel, xlabel = xlabel)
     return fig, ax
 
+# prepare chart (place legend, layout) for saving to pdf
 def save_chart_to_file(fig, pp):
-    fig.legend(loc='upper right', bbox_to_anchor=(0.95, 0.95))
+    fig.legend(loc="upper right", bbox_to_anchor=(0.95, 0.95))
+    fig.tight_layout()
+    pp.savefig(fig)
+
+# encapsulate in functions all the created charts
+########################################################################
+def create_title_page(xsize, ysize, pp, title):
+    fig, ax = plt.subplots(figsize = (xsize, ysize))
+    ax.axis("off")
+    fig.text(0.5, 0.5, title, ha = "center", va = "center", fontsize = 28)
+    pp.savefig()
+
+def create_histogram_chart(xsize, ysize, pp, df_mean, df_min, df_max, title, xlabel):
+    fig, ax = plt.subplots(1, 3, figsize = (xsize, ysize))
+    ax[0].set(title = title + " mean histogram", xlabel = xlabel, ylabel = "Frequency (days)")
+    ax[1].set(title = title + " min histogram", xlabel = xlabel, ylabel = "Frequency (days)")
+    ax[2].set(title = title + " max histogram", xlabel = xlabel, ylabel = "Frequency (days)")
+
+    df_mean["value"].hist(ax = ax[0], bins = int(df_max["value"].max() - df_min["value"].min()))
+    df_min["value"].hist(ax = ax[1], bins = int(df_max["value"].max() - df_min["value"].min()))
+    df_max["value"].hist(ax = ax[2], bins = int(df_max["value"].max() - df_min["value"].min()))
+
     fig.tight_layout()
     pp.savefig(fig)
 
 def create_indoor_outdoor_chart(xsize, ysize, pp, data_outdoor_mean, data_indoor_mean):
     fig, ax1 = init_chart("Indoor-Outdoor temperature", "Time", "Celsius Degrees", xsize, ysize)
 
-    data_outdoor_mean['value'].plot(ax = ax1, label = "Outdoor")
-    data_indoor_mean['value'].plot(ax = ax1, label = "Indoor")
+    data_outdoor_mean["value"].plot(ax = ax1, label = "Outdoor")
+    data_indoor_mean["value"].plot(ax = ax1, label = "Indoor")
 
     save_chart_to_file(fig, pp)
 
 def create_heat_index_chart(xsize, ysize, pp, data_heatindex):
     fig, ax1 = init_chart("Outdoor Heat index", "Time", "Celsius Degrees", xsize, ysize)
-
     start = 0
-    for index in range(0, len(data_heatindex['color']) - 1):
-        if (data_heatindex['color'][index] != data_heatindex['color'][index + 1]) or (index == len(data_heatindex['color']) - 2):
+    legend_labels = set()
+    for index in range(0, len(data_heatindex["color"]) - 1):
+        if (data_heatindex["color"][index] != data_heatindex["color"][index + 1]) or (index == len(data_heatindex["color"]) - 2):
             end = index + 1
-            ax1.fill_between(data_heatindex.index[start : end + 1], data_heatindex['value'][start : end + 1], step="post", color=data_heatindex['color'][start], label='')
+            if data_heatindex["color"][index] not in legend_labels:
+                color = data_heatindex["color"][index]
+                legend_labels.add(color)
+                if color == "green":
+                    my_label = "< 27"
+                elif color == "yellow":
+                    my_label = "[27:32]"
+                elif color == "orange":
+                    my_label = "[32:41]"
+                elif color == "red":
+                    my_label = "> 41"
+            else:
+                my_label = ""
+            ax1.fill_between(data_heatindex.index[start : end + 1], data_heatindex["value"][start : end + 1], step = "post", color = data_heatindex["color"][start], label = my_label)
             start = index + 1
 
     save_chart_to_file(fig, pp)
 
-def create_indoor_min_max_chart(xsize, ysize, pp, data_indoor_mean, data_indoor_max, data_indoor_min):
-    fig, ax1 = init_chart("Indoor MIN/MAX temperature", "Time", "Celsius Degrees", xsize, ysize)
+def create_temp_min_max_chart(xsize, ysize, pp, name, data_temp_mean, data_temp_max, data_temp_min):
+    fig, ax1 = init_chart(name + " MIN/MAX temperature", "Time", "Celsius Degrees", xsize, ysize)
 
-    ax1.fill_between(data_indoor_max.index, data_indoor_max['value'], data_indoor_min['value'], label = "Min-Max")
-    ax1.plot(data_indoor_max.index, data_indoor_mean['value'], color = 'white', label = "Mean")
+    ax1.fill_between(data_temp_max.index, data_temp_max["value"], data_temp_min["value"], label = "Min-Max")
+    ax1.plot(data_temp_max.index, data_temp_mean["value"], color = "white", label = "Mean")
 
     save_chart_to_file(fig, pp)
 
 def create_humidity_pressure_chart(xsize, ysize, pp, data_humidity_mean, data_pressure_mean):
     fig, ax1 = init_chart("Humidity-Pressure", "Time", "Celsius Degrees", xsize, ysize)
     ax2 = ax1.twinx()
-    ax2.set_ylabel('Pa', color = 'tab:orange')
+    ax2.set_ylabel("Pa", color = "tab:orange")
 
-    data_humidity_mean['value'].plot(ax = ax1, label = "Humidity")
-    data_pressure_mean['value'].plot(ax = ax2, color = 'tab:orange', label = "Pressure")
+    data_humidity_mean["value"].plot(ax = ax1, label = "Humidity")
+    data_pressure_mean["value"].plot(ax = ax2, color = "tab:orange", label = "Pressure")
 
     save_chart_to_file(fig, pp)
 
 def create_windows_chart(xsize, ysize, pp, data_window_left, data_window_right, data_outdoor_mean):
     fig, ax1 = init_chart("Window Living Left", "Time", "Open hours", xsize, ysize)
     ax2 = ax1.twinx()
-    ax2.set_ylabel('Celsius Degrees', color = 'tab:orange')
+    ax2.set_ylabel("Celsius Degrees", color = "tab:orange")
 
-    data_window_left['state'].plot(ax = ax1, label = "Window LEFT")
-    data_window_right['state'].plot(ax = ax1, label = "Window RIGHT")
+    data_window_left["state"].plot(ax = ax1, label = "Window LEFT")
+    data_window_right["state"].plot(ax = ax1, label = "Window RIGHT")
     # ~ ax1.axhline(y = 30 * 24, label = "Max")
-    data_outdoor_mean['value'].plot(ax = ax2, color = 'tab:orange', label = "Outdoor temp")
+    data_outdoor_mean["value"].plot(ax = ax2, color = "tab:orange", label = "Outdoor temp")
 
     save_chart_to_file(fig, pp)
+
+def create_table_samples(xsize, ysize, pp, df):
+    fig, ax = plt.subplots(2, figsize=(xsize, ysize))
+    fig.suptitle("Sample Statistics")
+    ax[0].axis("off")
+    ax[1].axis("off")
+
+    r,c = df.shape
+    ax[0].table(cellText = df.values, rowLabels = df.index, colLabels = df.columns, colColours = ["lightgray"] * c, bbox=[0,0,1,1])
+    pp.savefig(fig)
--- sensor_data_analysis.py
+++ sensor_data_analysis.py
@@ -2,14 +2,14 @@ import argparse
 import json
 import pymysql
 import pandas as pd
-from sshtunnel import SSHTunnelForwarder
-from matplotlib.backends.backend_pdf import PdfPages
-
 import numpy as np
 import matplotlib
+from matplotlib.backends.backend_pdf import PdfPages
+from sshtunnel import SSHTunnelForwarder
 
 import chart_functions
 
+# returns a string with the full query containing the date filtering (year, month, day)
 def get_query(table, filter_dict):
     query = "SELECT * FROM %s" % table
 
@@ -35,119 +35,131 @@ def get_query(table, filter_dict):
     query = "%s;" % query
     return query
 
+# returns a data frame based on the table the data is stored in MySQL and the date filter (to form the query)
 def get_data_from_sql(table, filter_dict, conn):
     query = get_query(table, filter_dict)
     print("query = %s" % query)
-    return pd.read_sql_query(query, conn, index_col='date')
+    return pd.read_sql_query(query, conn, index_col="date")
 
+# used to clean the data (trim values lower or higher than some tresholds) and return the resampled data for mean, min and max
 def get_clean_data(df, resample_rule, my_filter = None):
     if my_filter != None:
         assert (len(my_filter) == 2), "Number of filter items must be 2 (lower limit and upper limit)"
         assert (my_filter[0] <= my_filter[1]), "Bad argumets: %d must be lower or equal to %d" % (my_filter[0], my_filter[1])
-        df['value'] = df['value'].apply(lambda x: np.NaN if x < my_filter[0] or x > my_filter[1] else x)
+        df["value"] = df["value"].apply(lambda x: np.NaN if x < my_filter[0] or x > my_filter[1] else x)
     # the sensors might produce spikes and we can end up with the same date (which created problems when we use it as index)
     # remove index duplicates and create equal spaced samples (one per hour)
-    df = df[~df.index.duplicated(keep='last')]
-    df = df.resample('H').ffill();
-    df['value'] = df['value'].interpolate(method ='linear')
+    df = df[~df.index.duplicated(keep = "last")]
+    df = df.resample("H").ffill();
+    df["value"] = df["value"].interpolate(method = "linear")
 
     return df.resample(resample_rule).mean(), df.resample(resample_rule).min(), df.resample(resample_rule).max()
 
+# transform the unqual spaced data (for windows) to equal spaced data (which has been cleaned and resampled)
 def get_clean_equal_spaced_data(df, resample_rule):
     # the sensors might produce spikes and we can end up with the same date (which created problems when we use it as index)
     # remove index duplicates and create equal spaced samples (one per hour)
-    df = df[~df.index.duplicated(keep='last')]
-    df = df.resample('H').ffill()
+    df = df[~df.index.duplicated(keep="last")]
+    df = df.resample("H").ffill()
 
     # create a sample per day which sums the 1hour equaly spaced samples
     return df.resample(resample_rule).sum()
 
+# the heatindex is a how humidity influences the felt temperature
 def compute_heat_index(df_temperature, df_humidity, c):
+    # join the outdoor temp data with the humidity to start calculations
     data_heatindex = data_outdoor_mean.join(data_humidity_mean, on = "date", lsuffix = "temp", rsuffix = "humid")
-    data_heatindex['valuetemp_f'] = (data_heatindex['valuetemp'] * 9 / 5) + 32
-    data_heatindex['value'] = np.where(data_heatindex['valuetemp'] >= 21,
-        (c['c1'] + c['c2'] * data_heatindex['valuetemp_f'] + \
-        c['c3'] * data_heatindex['valuehumid'] + c['c4'] * data_heatindex['valuetemp_f'] * data_heatindex['valuehumid'] + \
-        c['c5'] * (data_heatindex['valuetemp_f'] ** 2) + c['c6'] * (data_heatindex['valuehumid'] ** 2) + \
-        c['c7'] * (data_heatindex['valuetemp_f'] ** 2) * data_heatindex['valuehumid'] + \
-        c['c8'] * data_heatindex['valuetemp_f'] * (data_heatindex['valuehumid'] ** 2) + \
-        c['c9'] * (data_heatindex['valuetemp_f'] ** 2) * (data_heatindex['valuehumid'] ** 2) - 32) * 5/9 , data_heatindex['valuetemp'])
-    data_heatindex = data_heatindex.loc[str(year) + '-05-01' : str(year) + '-10-01']
+    # the applied formula is for F degees while the measurments are in C degrees
+    data_heatindex["valuetemp_f"] = (data_heatindex["valuetemp"] * 9 / 5) + 32
+    data_heatindex["value"] = np.where(data_heatindex["valuetemp"] >= 21,
+        (c["c1"] + c["c2"] * data_heatindex["valuetemp_f"] + \
+        c["c3"] * data_heatindex["valuehumid"] + c["c4"] * data_heatindex["valuetemp_f"] * data_heatindex["valuehumid"] + \
+        c["c5"] * (data_heatindex["valuetemp_f"] ** 2) + c["c6"] * (data_heatindex["valuehumid"] ** 2) + \
+        c["c7"] * (data_heatindex["valuetemp_f"] ** 2) * data_heatindex["valuehumid"] + \
+        c["c8"] * data_heatindex["valuetemp_f"] * (data_heatindex["valuehumid"] ** 2) + \
+        c["c9"] * (data_heatindex["valuetemp_f"] ** 2) * (data_heatindex["valuehumid"] ** 2) - 32) * 5/9 , data_heatindex["valuetemp"])
+    # heatindex makes sense to be applied when temperatures are high May-Octomber
+    data_heatindex = data_heatindex.loc[str(year) + "-05-01" : str(year) + "-10-01"]
 
-    ## Apply color labels
-    data_heatindex.loc[(data_heatindex['value'] < 27), 'color'] = 'green'
-    data_heatindex.loc[(data_heatindex['value'] >= 27) & (data_heatindex['value'] < 32), 'color'] = 'yellow'
-    data_heatindex.loc[(data_heatindex['value'] >= 32) & (data_heatindex['value'] < 41), 'color'] = 'orange'
-    data_heatindex.loc[(data_heatindex['value'] >= 41), 'color'] = 'red'
+    ## apply color labels in order to be able to know the color of the segements when creating the chart
+    data_heatindex.loc[(data_heatindex["value"] < 27), "color"] = "green"
+    data_heatindex.loc[(data_heatindex["value"] >= 27) & (data_heatindex["value"] < 32), "color"] = "yellow"
+    data_heatindex.loc[(data_heatindex["value"] >= 32) & (data_heatindex["value"] < 41), "color"] = "orange"
+    data_heatindex.loc[(data_heatindex["value"] >= 41), "color"] = "red"
 
     return data_heatindex
 
+# read configuration
 parser = argparse.ArgumentParser()
-parser.add_argument('--config_file', '-f', default="analysis_config.json", help='path to configuration json file (default: analysis_config.json)')
+parser.add_argument("--config_file", "-f", default="analysis_config.json", help="path to configuration json file (default: analysis_config.json)")
 args = parser.parse_args()
 
 with open(args.config_file) as data_file:
     config_options = json.load(data_file)
 data_file.close()
 
-year = 2017
-
-pp = PdfPages('sensors_analysis_report_' + str(year) + '.pdf')
-
 matplotlib.style.use(config_options["charts"]["style"])
 
-#get raw data from SQL
-########################################################################
-with SSHTunnelForwarder((config_options["ssh"]["server"], config_options["ssh"]["port"]), ssh_username = config_options["ssh"]["user"],
-    ssh_password = config_options["ssh"]["password"], remote_bind_address = (config_options["mysql"]["server"], config_options["mysql"]["port"])) as tunnel:
+for year in range(config_options["charts"]["start_report_year"], config_options["charts"]["end_report_year"] + 1):
 
-    conn = pymysql.connect(host = config_options["mysql"]["server"], user = config_options["mysql"]["user"], passwd = config_options["mysql"]["password"],
-        db = config_options["mysql"]["database"], port = tunnel.local_bind_port)
+    pp = PdfPages("sensors_analysis_report_" + str(year) + ".pdf")
 
-    date_filter = {"year": [year, year]}#, "month": [8, 8], "day": [21, 21]}
+    #get raw data from SQL via SSH -> to be removed and replaced with REST call (if there is a REST server)
+    ########################################################################
+    with SSHTunnelForwarder((config_options["ssh"]["server"], config_options["ssh"]["port"]), ssh_username = config_options["ssh"]["user"],
+        ssh_password = config_options["ssh"]["password"], remote_bind_address = (config_options["mysql"]["server"], config_options["mysql"]["port"])) as tunnel:
 
-    data_temperature = get_data_from_sql("temperature", date_filter, conn)
-    data_humidity = get_data_from_sql("humidity", date_filter, conn)
-    data_pressure = get_data_from_sql("pressure", date_filter, conn)
-    data_windows = get_data_from_sql("windows", date_filter, conn)
+        conn = pymysql.connect(host = config_options["mysql"]["server"], user = config_options["mysql"]["user"], passwd = config_options["mysql"]["password"],
+            db = config_options["mysql"]["database"], port = tunnel.local_bind_port)
 
-    conn.close()
+        date_filter = {"year": [year, year]}#, "month": [8, 8], "day": [21, 21]}
 
-#process and clear SQL data (prepare for charts)
-########################################################################
-data_outdoor = data_temperature.loc[data_temperature['name'] == 'Outdoor Unit']
-data_indoor = data_temperature.loc[data_temperature['name'] == 'Indoor Living']
-data_window_left = data_windows.loc[data_windows['name'] == 'Living LEFT']
-data_window_right = data_windows.loc[data_windows['name'] == 'Living RIGHT']
+        data_temperature = get_data_from_sql("temperature", date_filter, conn)
+        data_humidity = get_data_from_sql("humidity", date_filter, conn)
+        data_pressure = get_data_from_sql("pressure", date_filter, conn)
+        data_windows = get_data_from_sql("windows", date_filter, conn)
 
-data_window_left = get_clean_equal_spaced_data(data_window_left, 'M')
-data_window_right = get_clean_equal_spaced_data(data_window_right, 'M')
+        conn.close()
 
-data_outdoor_mean, data_outdoor_min, data_outdoor_max = get_clean_data(data_outdoor, 'D')
-data_outdoor_mean_month, data_outdoor_min_month, data_outdoor_max_month = get_clean_data(data_outdoor, 'M')
-data_indoor_mean, data_indoor_min, data_indoor_max = get_clean_data(data_indoor, 'D', [15, 40])
-data_humidity_mean, data_humidity_min, data_humidity_max = get_clean_data(data_humidity, 'D', [20, 100])
-data_pressure_mean, data_pressure_min, data_pressure_max = get_clean_data(data_pressure, 'D', [900, 1100])
+    #process and clear SQL data (prepare for charts)
+    ########################################################################
+    data_outdoor = data_temperature.loc[data_temperature["name"] == "Outdoor Unit"]
+    data_indoor = data_temperature.loc[data_temperature["name"] == "Indoor Living"]
+    data_window_left = data_windows.loc[data_windows["name"] == "Living LEFT"]
+    data_window_right = data_windows.loc[data_windows["name"] == "Living RIGHT"]
 
-c = config_options["charts"]["heat_index_coef"]
-data_heatindex = compute_heat_index(data_outdoor_mean, data_humidity_mean, c)
+    sample_statistics = pd.DataFrame(columns=["Outdoor temp", "Indoor temp", "Humidity", "Pressure", "Window Left", "Window Right"])
+    sample_statistics.loc["Collected samples"] = [len(data_outdoor), len(data_indoor), len(data_humidity), len(data_pressure), len(data_window_left), len(data_window_right)]
 
-#create charts
-########################################################################
-xsize = config_options["charts"]["xfig_size"]
-ysize = config_options["charts"]["yfig_size"]
-chart_functions.create_indoor_outdoor_chart(xsize, ysize, pp, data_outdoor_mean, data_indoor_mean)
-chart_functions.create_heat_index_chart(xsize, ysize, pp, data_heatindex)
-chart_functions.create_indoor_min_max_chart(xsize, ysize, pp, data_indoor_mean, data_indoor_max, data_indoor_min)
-chart_functions.create_humidity_pressure_chart(xsize, ysize, pp, data_humidity_mean, data_pressure_mean)
-chart_functions.create_windows_chart(xsize, ysize, pp, data_window_left, data_window_right, data_outdoor_mean_month)
+    data_window_left = get_clean_equal_spaced_data(data_window_left, "M")
+    data_window_right = get_clean_equal_spaced_data(data_window_right, "M")
 
-#############
-# ~ correlation = []
-# ~ window_size = 20
-# ~ for index in range(0, len(data_pressure['value']) - window_size - 1):
-    # ~ correlation.append(data_pressure_mean['value'][index : index + window_size].corr(data_humidity_mean['value'][index : index + window_size]))
-# ~ fig, ax1 = plt.subplots()
-# ~ ax1.plot(correlation)
+    data_outdoor_mean, data_outdoor_min, data_outdoor_max = get_clean_data(data_outdoor, "D")
+    data_outdoor_mean_month, data_outdoor_min_month, data_outdoor_max_month = get_clean_data(data_outdoor, "M")
+    data_indoor_mean, data_indoor_min, data_indoor_max = get_clean_data(data_indoor, "D", [config_options["data"]["min_indoor_temp"], config_options["data"]["max_indoor_temp"]])
+    data_humidity_mean, data_humidity_min, data_humidity_max = get_clean_data(data_humidity, "D", [config_options["data"]["min_humidity"], config_options["data"]["max_humidity"]])
+    data_pressure_mean, data_pressure_min, data_pressure_max = get_clean_data(data_pressure, "D", [config_options["data"]["min_pressure"], config_options["data"]["max_pressure"]])
 
-pp.close()
+    c = config_options["charts"]["heat_index_coef"]
+    data_heatindex = compute_heat_index(data_outdoor_mean, data_humidity_mean, c)
+
+    sample_statistics.loc["Used samples"] = [len(data_outdoor_mean), len(data_indoor_mean), len(data_humidity_mean), len(data_pressure_mean), len(data_window_left), len(data_window_right)]
+    sample_statistics.loc["Min"] = ["%.1f" % data_outdoor_min["value"].min(), "%.1f" % data_indoor_min["value"].min(), data_humidity_min["value"].min(), data_pressure_min["value"].min(), "-", "-"]
+    sample_statistics.loc["Max"] = ["%.1f" % data_outdoor_max["value"].max(), "%.1f" % data_indoor_max["value"].max(), data_humidity_max["value"].max(), data_pressure_max["value"].max(), "-", "-"]
+
+    #create charts
+    ########################################################################
+    xsize = config_options["charts"]["xfig_size"]
+    ysize = config_options["charts"]["yfig_size"]
+    chart_functions.create_title_page(xsize, ysize, pp, "Sensor data report - " + str(year))
+    chart_functions.create_table_samples(xsize, ysize, pp, sample_statistics)
+    chart_functions.create_histogram_chart(xsize, ysize, pp, data_outdoor_mean, data_outdoor_min, data_outdoor_max, "Outdoor temperature", "Celsius Degrees")
+    chart_functions.create_indoor_outdoor_chart(xsize, ysize, pp, data_outdoor_mean, data_indoor_mean)
+    chart_functions.create_temp_min_max_chart(xsize, ysize, pp, "Indoor", data_indoor_mean, data_indoor_max, data_indoor_min)
+    chart_functions.create_temp_min_max_chart(xsize, ysize, pp, "Outdoor", data_outdoor_mean, data_outdoor_max, data_outdoor_min)
+    chart_functions.create_heat_index_chart(xsize, ysize, pp, data_heatindex)
+    chart_functions.create_humidity_pressure_chart(xsize, ysize, pp, data_humidity_mean, data_pressure_mean)
+    chart_functions.create_windows_chart(xsize, ysize, pp, data_window_left, data_window_right, data_outdoor_mean_month)
+
+    pp.close()
+    print("Finished creating sensors_analysis_report_" + str(year) + ".pdf")
